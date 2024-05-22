520ace997a395e0a05fc437f9e26e570d77eb42a
Kyon <9278551+KyonCN@users.noreply.github.com>
2024-05-23 00:58:06 +0800

    Temporary Fix: Adguard

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -3,5 +3,6 @@
 #更改默认地址为192.168.8.1
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-
+# 更改默认 Shell 为 zsh
+sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
