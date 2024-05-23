dfddc45616aee95bdb964f39d937ed0e5a1be1af
Kyon <9278551+KyonCN@users.noreply.github.com>
2024-05-23 23:05:32 +0800

    update

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -7,9 +7,11 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 pwd
-ls .
-ls ..
-ls ../..
-ls /workdir
+
+
+ sudo -E apt-get -qq update -y locate
+ sudo -E updatedb
+ locate files/etc/config/smartdns
+
 # 导入配置文件
 cp -R -f files/* package/base-files/files/
