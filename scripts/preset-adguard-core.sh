0dd07b507bbc8172ae3244572986dd733ec32e0c
Kyon <9278551+KyonCN@users.noreply.github.com>
2024-05-23 22:23:58 +0800

    adguard

--- scripts/preset-adguard-core.sh
+++ scripts/preset-adguard-core.sh
@@ -4,8 +4,8 @@
 mkdir -p files/usr/bin/
 AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_${1} | awk -F '"' '{print $4}')
 
-# wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome/AdGuardHome
+wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome
 # Temporary Fix
-wget -qO- https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.47/AdGuardHome_linux_amd64.tar.gz | tar xOvz > files/usr/bin/AdGuardHome
+#wget -qO- https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.47/AdGuardHome_linux_amd64.tar.gz | tar xOvz > files/usr/bin/AdGuardHome
 
 chmod +x files/usr/bin/AdGuardHome
